120f404692e6c848bcd8d6fbe0488e9cc8f76311
Bas van den Berg <bas@dib0.nl>
2025-01-17 13:52:50 +0100

    Update __init__.py adding a binary sensor

--- custom_components/network_scanner/__init__.py
+++ custom_components/network_scanner/__init__.py
@@ -1,18 +1,18 @@
-from .sensor import NetworkScanner
 from .const import DOMAIN
 
 async def async_setup(hass, config):
-    """Set up the Network Scanner component."""
-    # Store YAML configuration in hass.data
+    """Set up the Network Scanner component from YAML (if any)."""
     hass.data[DOMAIN] = config.get(DOMAIN, {})
     return True
 
 async def async_setup_entry(hass, config_entry):
     """Set up Network Scanner from a config entry."""
-    await hass.config_entries.async_forward_entry_setups(config_entry, ["sensor"])
+    # Forward this config entry to the sensor and binary_sensor platforms
+    await hass.config_entries.async_forward_entry_setups(config_entry, ["sensor", "binary_sensor"])
     return True
 
 async def async_unload_entry(hass, config_entry):
     """Unload a config entry."""
-    await hass.config_entries.async_forward_entry_unload(config_entry, "sensor")
-    return True
+    unload_ok = await hass.config_entries.async_forward_entry_unload(config_entry, "sensor")
+    unload_ok = unload_ok and await hass.config_entries.async_forward_entry_unload(config_entry, "binary_sensor")
+    return unload_ok
