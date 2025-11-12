a08099d65537806835ce36fa988c4e4b52f61d1a
Paul Gale <paul.gale@pagabv.com>
2025-11-12 16:41:27 +0000

    v0.12.5

--- custom_components/network_scanner/provider/__init__.py
+++ custom_components/network_scanner/provider/__init__.py
@@ -4,7 +4,8 @@ This module exports individual provider implementations so that the
 coordinator can import them lazily.  Additional providers should be
 added here and referenced by their name in :mod:`network_scanner.const`.
 """
+from . import opnsense  # noqa: F401
+from . import unifi     # noqa: F401
+from . import adguard   # noqa: F401
 
-from .opnsense import async_get_devices as opnsense
-
-__all__ = ["opnsense"]
+__all__ = ["opnsense", "unifi", "adguard"]
