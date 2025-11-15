afecfb587a9057c5d7858e0a1ecfc865083d0434
Paul Gale <paul.gale@pagabv.com>
2025-11-15 20:10:25 +0000

    v.0.15.3

--- custom_components/network_scanner/const.py
+++ custom_components/network_scanner/const.py
@@ -20,6 +20,11 @@ CONF_NAME: Final = "name"          # Username (UniFi/AdGuard)
 CONF_PASSWORD: Final = "password"
 CONF_TOKEN: Final = "token"        # UniFi API token
 
+# Auth mode (used by config_flow)
+CONF_AUTH_MODE: Final = "auth_mode"
+AUTH_MODE_PASSWORD: Final = "password"
+AUTH_MODE_TOKEN: Final = "token"
+
 # Misc options
 CONF_VERIFY_SSL: Final = "verify_ssl"
 CONF_INTERVAL_MIN: Final = "interval_min"
@@ -31,5 +36,9 @@ DEFAULT_OPTIONS: Dict[str, Any] = {
     CONF_INTERVAL_MIN: 3,
 }
 
-# Heuristic constant (kept for potential use)
+# Heuristic constant
 ASSUMED_ARP_TTL_S: Final = 1200
+
+# Optional helper tuples your config_flow may import
+PROVIDERS: tuple[str, ...] = ("opnsense", "unifi", "adguard", "opnsense_unifi")
+AUTH_MODES: tuple[str, ...] = (AUTH_MODE_PASSWORD, AUTH_MODE_TOKEN)
