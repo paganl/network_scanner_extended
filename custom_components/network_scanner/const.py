97020e3908b280d51645e1c18373fa1d68965f7a
Paul Gale <paul.gale@pagabv.com>
2025-11-15 19:26:19 +0000

    v0.15.0

--- custom_components/network_scanner/const.py
+++ custom_components/network_scanner/const.py
@@ -2,9 +2,9 @@ from __future__ import annotations
 
 DOMAIN = "network_scanner"
 
-CONF_URL        = "url"         # generic (still supported)
 CONF_OPNSENSE_URL = "opnsense_url"
 CONF_UNIFI_URL    = "unifi_url"
+CONF_ADGUARD_URL = 'adguard url" 
 
 CONF_KEY        = "key"         # OPNsense
 CONF_SECRET     = "secret"      # OPNsense
@@ -17,14 +17,6 @@ CONF_PROVIDER   = "provider"    # "opnsense" | "unifi" | "adguard" | "opnsense_u
 
 DEFAULT_OPTIONS = {
     CONF_PROVIDER: "opnsense",
-    CONF_URL: "",
-    CONF_OPNSENSE_URL: "",
-    CONF_UNIFI_URL: "",
-    CONF_KEY: "",
-    CONF_SECRET: "",
-    CONF_NAME: "",
-    CONF_PASSWORD: "",
-    CONF_TOKEN: "",
     CONF_VERIFY_SSL: False,
     CONF_INTERVAL_MIN: 3,
 }
